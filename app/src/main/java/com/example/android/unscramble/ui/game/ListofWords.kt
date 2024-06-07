fa890d89890f8a059202a8e89e387692d6e32796
RidhaniSetiadi <ridhanisetiadi08@gmail.com>
2024-06-08 00:07:05 +0800

    dhani prak 3

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,34 @@
+package com.example.android.unscramble.ui.game
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("Hewan", "otomatis", "anekdot", "alfabet", "semua", "keren", "muncul",
+        "balon", "keranjang", "bangku", "terbaik", "ulang tahun", "buku", "koper",
+        "kamera", "berkemah", "lilin", "kucing", "kembang kol", "obrolan", "anak",
+        "kelas", "klasik", "ruang kelas", "kopi", "warna warni", "kue kering",
+        "kreatif", "pesiar", "menari", "siang hari", "dinosaurus", "gagang pintu",
+        "makan malam", "mimpi", "senja", "makan", "gajah", "zamrud", "menyeramkan",
+        "listrik", "selesai", "bunga", "mengikuti", "rubah", "bingkai", "gratis/bebas",
+        "sering", "corong", "hijau", "gitar", "barang dagangan", "kaca", "hebat",
+        "cekikikan", "potongan rambut", "setengah", "buatan sendiri", "terjadi",
+        "madu", "tergesa gesa", "seratus", "es", "igloo", "investasi", "mengundang",
+        "ikon", "memperkenalkan", "lelucon", "ceria", "jurnal", "melompat", "bergabung",
+        "kanguru", "papan ketik", "dapur", "koala", "baik", "kaleidoskop", "pemandangan",
+        "terlambat", "tertawa", "belajar", "lemon", "surat", "lilin", "majalah", "laut",
+        "marshmallow", "labirin", "meditasi", "melodi", "menit", "monumen", "bulan",
+        "sepeda motor", "gunung", "musik", "utara", "hidung", "malam", "nama", "tidak pernah",
+        "bernegosiasi", "nomor", "berlawanan", "gurita", "ek", "pesanan", "terbuka", "kutub",
+        "bungkus", "lukisan", "orang", "piknik", "bantal", "pizza", "siaran podcast", "yoyo",
+        "anjing kecil", "teka-teki", "resep", "melepaskan", "restoran", "berputar", "mundur",
+        "ruangan", "berlari", "rahasia", "biji", "kapal", "kemeja", "seharusnya", "kecil","jalan",
+        "pesawat luar angkasa", "melihat bintang", "keterampilan", "wisatawan", "bepergian", "yoga",
+        "gaya", "matahari terbit", "taksi", "rapi", "pengatur waktu", "bersama-sama", "gigi",
+        "truk", "dibawah", "berguna", "unicorn", "unik", "membangkitkan semangat", "seragam",
+        "vas", "biola", "pengunjung", "visi", "volume", "pandangan", "walrus", "berkeliaran", "dunia",
+        "musim dingin", "pusaran angin", "sinar-X", "silofon", "yogurt", "kamu", "tahun", "lezat",
+        "zebra", "zigzag", "zoologi", "zona", "semangat", "presentasi", "sumur","dunia")
+
+
